db9a691cd6601c81c04f3637cdbc00c0cc27c3e2
Nguyendat0908 <Nguyendat090803@gmail.com>
2025-02-27 22:18:43 +0700

    Initial project

---
+++ src/main/java/com/example/NHD_BOOK_SHOP/NhdBookShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.NHD_BOOK_SHOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhdBookShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhdBookShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NHD_BOOK_SHOP/NhdBookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NHD_BOOK_SHOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhdBookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
